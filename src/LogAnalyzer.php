051da2eadcc3144fcede4acb1f64ea114ccdd144
Winnie Lin <onecentlin@gmail.com>
2018-06-13 19:21:04 +0800

    Add LogAnalyzer.php

---
+++ src/LogAnalyzer.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Winnie\LaraDebut;
+
+class LogAnalyzer
+{
+    public function isValidLogFileName(string $fileName)
+    {
+        if (substr($fileName, -4) === ".SLF") {
+            return false;
+        }
+        return true;
+    }
+}
